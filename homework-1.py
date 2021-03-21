a785d4ffdebc92386cc0bed2982b4d33fa67fa28
aakashgenani <aakashgenani@gmail.com>
2021-03-21 20:18:10 +0100

    Update homework-1.py

--- homework-1.py
+++ homework-1.py
@@ -2,7 +2,7 @@
 # Homework 1
 # Question 1 & 2
 def check_password(password):
-    if len(password) < 9:
+    if len(password) < 8:
         print('Error: Minimum 8 characters required!')
     if not any(i.isupper() for i in password):
         print('Error: At least one letter should be uppercase!')
@@ -32,37 +32,40 @@ def check_freq(sep_words):
     return c_final
 
 
-def sort_words(freq_word):
-    sorted_words = freq_word
-    sorted_words.sort(key=lambda x: x[1], reverse=True)
-    return sorted_words
+def arrange_alphabetically(sep_words):  # arranging words with frequency but alphabetically when frequency is same
+    sorted_alpha = sorted(sep_words)
+    sorted_list = sorted(sorted_alpha, key=lambda c: sep_words.count(c), reverse=True)
+    return sorted_list
 
 
-def most_freq_words(sorted_words):
-    result = [sorted_words[0], sorted_words[1]]
-    result.sort()
+def find_most_freq_words(sorted_alpha):  # finding the most frequent word(s)
+    a = [sorted_alpha.count(x) for x in sorted_alpha]  # set of counts of the elements
+    set_most_freq_words = set([sorted_alpha[i] for i in range(len(a)) if a[i] == max(a)])  # using max counts as index
+    result = sorted(set_most_freq_words)  # arranging the set alphabetically
     return result
 
 
-def str_most_freq_words(most_freq_word):
-    result = f"{most_freq_word[0][0]} {most_freq_word[1][0]}"
-    return result
+def make_str_form(most_freq_words):
+    s = str()
+    for i in range(len(most_freq_words)):
+        a = most_freq_words[i]
+        s = s + a + ' '
+    return s
 
 
 def main():
-    password = str(input("Enter password: "))  # Ask user to enter a password
+    password = str(input("Ques 1. Enter password: "))  # Ask user to enter a password
     check_password(password)
-    sent = str(input('Enter sentence: '))       # Ask use to enter a sentence
+    sent = str(input('Ques 2. Enter sentence: '))       # Ask use to enter a sentence
     sep_words = split_sent(sent)                # Call to the function which splits the sentence into list of words
-    print(f'List of separated words from the sentence: {sep_words}')
+    print(f'2)a) List of separated words from the sentence: {sep_words}')
     freq_word = check_freq(sep_words)
-    print(f'Words with their frequency: {freq_word}')
-    sorted_words = sort_words(freq_word)
-    print(f'Word with their frequency in alphabetical order: {sorted_words}')
-    most_freq_word = most_freq_words(sorted_words)
-    print(f'Two most frequent words with their frequency: {most_freq_word}')
-    str_most_freq_word = str_most_freq_words(most_freq_word)
-    print(f'Two most frequent words are: {str_most_freq_word}')
+    print(f'2)b) Words with their frequency: {freq_word}')
+    sorted_alpha = arrange_alphabetically(sep_words)
+    most_freq_words = find_most_freq_words(sorted_alpha)
+    print(f'2)c) Most frequent word(s) in alphabetical order: {most_freq_words}')
+    final_str_form = make_str_form(most_freq_words)
+    print(f'Final string form: {final_str_form}')
 
 
 if __name__ == "__main__":
